b55a79948350350a21dd165c87f31851af657a82
Nguyễn Trí Dũng <ntdungqlqb@ntdungqlqb.local>
2024-12-30 20:29:42 +0700

    Add Slick Carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+$(".service-list").slick({
+    infinite: !0,
+    slidesToShow: 5,
+    autoplay: !0,
+    autoplaySpeed: 3000,
+    speed: 1100,
+    arrows: !1,
+    dots: !0,
+});
 $(".counter").countUp();
